3606f93851ad30602657dcb7807da1c8d356bd92
gaoguanqi <307590625@qq.com>
2021-04-04 22:17:49 +0800

    fly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_ui/common/router/index.dart';
 import 'package:flutter_ui/common/theme/app_theme.dart';
@@ -7,6 +8,7 @@ import 'package:flutter_ui/global.dart';
 import 'package:flutter_ui/pages/home/home_page.dart';
 import 'package:flutter_ui/pages/welcome/welcome_page.dart';
 import 'package:get/get.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 void main() {
   Global.init().then((value) => runApp(MyApp()));
@@ -25,8 +27,31 @@ class MyApp extends StatelessWidget {
               initialRoute: '/',
               getPages: AppPages.pages,
              // home: SplashPage(),
-              home: Global.hasHome? HomePage(): WelcomePage(),
+             //  home: Global.hasHome? HomePage(): WelcomePage(),
               // home: Test1Page(),
+              home: RefreshConfiguration(
+                headerBuilder: () => WaterDropHeader(),
+                // 配置头部默认下拉刷新视图
+                footerBuilder: () => ClassicFooter(),
+                // 配置底部默认上拉加载视图
+                headerTriggerDistance: 80.0,
+                // 头部触发刷新的距离
+                springDescription:
+                SpringDescription(stiffness: 170, damping: 16, mass: 1.9),
+                // 自定义弹回动画
+                maxOverScrollExtent: 100,
+                //The maximum dragging range of the head. Set this property if a rush out of the view area occurs
+                maxUnderScrollExtent: 0,
+                // Maximum dragging range at the bottom
+                enableScrollWhenRefreshCompleted: true,
+                //This property is incompatible with PageView and TabBarView. If you need TabBarView to slide left and right, you need to set it to true.
+                enableLoadingWhenFailed: true,
+                //In the case of load failure, users can still trigger more loads by gesture pull-up.
+                hideFooterWhenNotFull: false,
+                // Disable pull-up to load more functionality when Viewport is less than one screen
+                enableBallisticLoad: true,
+                child: Global.hasHome? HomePage(): WelcomePage(),
+              ),
             ));
   }
 }
--- lib/pages/home/nav/fly.dart
+++ lib/pages/home/nav/fly.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class PageFly extends StatelessWidget {
+
+  //每页显示数量
+  static const int PAGE_SIZE = 10;
+  //当前为第几页
+  int page = 1;
+  //是否加载过数据
+  bool loaded;
+  //是否允许上拉
+  bool _enablePullUp = true;
+  //listview数据源
+  List<String> _list = [
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+  ];
+  //刷新加载控制器
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      alignment: Alignment.center,
-      child: Text('fly'),
+    return SmartRefresher(
+      ///可在此通过header:和footer:指定个性效果
+      //允许下拉
+      enablePullDown: true,
+      //允许上拉加载
+      enablePullUp: _enablePullUp,
+      //控制器
+      controller: _refreshController,
+      //刷新回调方法
+      onRefresh: _onRefresh,
+      //加载下一页回调
+      onLoading: _onLoading,
+      child: ListView.builder(
+        itemBuilder: (c, i) => Text('ssss'),
+        itemCount: _list.length,
+      ),
     );
   }
+
+  void _onRefresh() async {
+    _loadData(true);
+  }
+
+  void _onLoading() async {
+    _loadData(false);
+  }
+
+  void _initData() async {
+    _loadData(true);
+  }
+
+  _loadData(final bool onRefresh) async {
+
+    await _list.add('value');
+  }
+
 }
